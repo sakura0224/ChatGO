e906e5110f53499ae27660021ea7e90f87f5ab4d
sakura0224 <shiinayane@gmail.com>
2024-03-14 10:37:30 +0800

    Add chat app and configure settings

--- ChatGO/asgi.py
+++ ChatGO/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for ChatGO project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatGO.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatGO.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+# http请求使用这个
+"http": get_asgi_application(),
+
+# websocket请求使用这个
+"websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
